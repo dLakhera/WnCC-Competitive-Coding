28f912bfb2c07ad3b032b0a34b4f3ad4ea4c7bd8
droidLakhera <dlakhera02@gmail.com>
2020-05-20 19:19:20 +0530

    Used my micro, code remains same

--- Session 2/dfs_recursive.cpp
+++ Session 2/dfs_recursive.cpp
@@ -1,4 +1,18 @@
+// #include<bits/stdc++.h>
 #include<bits/stdc++.h>
+#define ll long long
+#define MAX 1000003
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl "\n"
+#define all(a) (a).begin(),(a).end()
+#define input(a) for(int i=0;i<a.size();i++) cin>>a[i];
+#define test int t; cin>>t; while(t--)
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define ii pair<int,int>
+#define ff first
+#define ss second
+
 using namespace std ;
 
 const int N=100010;
